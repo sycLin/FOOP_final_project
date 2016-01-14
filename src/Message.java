34379540893838993fe01c8793635374751a216d
m516825 <b01902078@ntu.edu.tw>
2016-01-14 16:02:02 +0800

    new message type in Daifugo

--- src/Message.java
+++ src/Message.java
@@ -24,11 +24,12 @@ class Message {
 	public static final short ACTION_PLAYING	= (short)0b0000000000000001;
 	public static final short ACTION_WINNING	= (short)0b0000000000000010;
 	public static final short ACTION_LOSING		= (short)0b0000000000000100;
-	public static final short ACTION_LEADING	= (short)0b0000000000001000;
-	public static final short ACTION_NEW_ROUND	= (short)0b0000000000010000;
-	public static final short ACTION_ROUND_END	= (short)0b0000000000100000;
-	public static final short ACTION_CANT_BEAT	= (short)0b0000000001000000;
-	public static final short ACTION_WRONG_TYPE	= (short)0b0000000010000000;
+	public static final short ACTION_PASSING	= (short)0b0000000000001000;
+	public static final short ACTION_LEADING	= (short)0b0000000000010000;
+	public static final short ACTION_NEW_ROUND	= (short)0b0000000000100000;
+	public static final short ACTION_ROUND_END	= (short)0b0000000001000000;
+	public static final short ACTION_CANT_BEAT	= (short)0b0000000010000000;
+	public static final short ACTION_WRONG_TYPE	= (short)0b0000000100000000;
 
 	/**
 	 * to indicate the occasions of the game
